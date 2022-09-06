025e5a1e440e4e3e002dc7f6ca18f0ef3152b0f8
Lugas90 <Lugas90@mail.ru>
2022-09-06 18:05:56 +0300

    1 комит

---
+++ src/main/java/ru/hogvarts/school_3_3/School33Application.java
@@ -0,0 +1,13 @@
+package ru.hogvarts.school_3_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class School33Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(School33Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/hogvarts/school_3_3/School33ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hogvarts.school_3_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class School33ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
